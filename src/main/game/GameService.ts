474c1b033f3d326c4118b0a3888f60da57fcef9f
Leo22878 <prusevna@gmail.com>
2025-01-30 17:18:28 +0300

    релиз 0.6.0

--- src/main/game/GameService.ts
+++ src/main/game/GameService.ts
@@ -1,6 +1,8 @@
 import { Profile, Server } from '@aurora-launcher/core';
 import { Service } from '@freshgum/typedi';
-
+import { promises as fs } from 'fs'; // Импортируем модуль для работы с файловой системой
+import path from 'path'; // Импортируем модуль для работы с путями
+import { StorageHelper } from '../helpers/StorageHelper'; // Импортируем StorageHelper
 import { APIManager } from '../api/APIManager';
 import { GameWindow } from './GameWindow';
 import { LibrariesMatcher } from './LibrariesMatcher';
@@ -36,6 +38,29 @@ export class GameService {
         return this.selectedProfile;
     }
 
+    /**
+     * Очищает папку скинов перед запуском игры.
+     */
+    private async clearSkinsFolder(clientArgs: Profile) {
+        const skinsPath = path.join(StorageHelper.storageDir, 'assets', 'skins'); // Правильный путь к папке скинов
+        console.log('Путь к папке скинов:', skinsPath);
+        try {
+        // Проверяем, существует ли папка
+        await fs.access(skinsPath);
+
+        // Удаляем все содержимое папки
+        await fs.rm(skinsPath, { recursive: true, force: true });
+
+        console.log('Все содержимое папки скинов успешно удалено.');
+    } catch (error) {
+            if (error.code !== 'ENOENT') {
+                this.gameWindow.sendToConsole(`Ошибка при очистке папки скинов: ${error}`);
+            } else {
+                this.gameWindow.sendToConsole('Папка скинов отсутствует, пропускаем удаление.');
+            }
+        }
+    }
+
     async startGame() {
         const profile = this.selectedProfile;
         const server = this.selectedServer;
@@ -45,6 +70,9 @@ export class GameService {
             this.gameWindow.stopGame();
             return;
         }
+
+        // Очищаем папку скинов перед запуском игры
+        await this.clearSkinsFolder(profile);
 
         const libraries = profile.libraries.filter((library) =>
             LibrariesMatcher.match(library.rules),
